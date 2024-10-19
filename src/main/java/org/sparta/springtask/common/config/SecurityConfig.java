b12bfc2085abc926dab1f25c04eeb9d02776c2f3
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-19 15:59:13 +0900

    ★elasticsearch logstash kibana 적용!★

--- src/main/java/org/sparta/springtask/common/config/SecurityConfig.java
+++ src/main/java/org/sparta/springtask/common/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/*/user/**").permitAll()
                         .requestMatchers(HttpMethod.PATCH, "/api/*/users/authority").hasAuthority(UserRole.Authority.ADMIN)
                         .requestMatchers(HttpMethod.POST, "/api/*/workspaces").hasAuthority(UserRole.Authority.ADMIN)
-                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/actuator/prometheus").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
---
+++ src/main/java/org/sparta/springtask/common/health/ActuatorController.java
@@ -0,0 +1,14 @@
+package org.sparta.springtask.common.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/actuator")
+public class ActuatorController {
+    @GetMapping("/prometheus")
+    public String prometheus() {
+        return "UP";
+    }
+}
