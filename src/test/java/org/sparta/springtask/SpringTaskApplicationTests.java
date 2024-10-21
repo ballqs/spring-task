5cf71e43aa2af633b6ccf72fc683d66c7b002b67
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-22 00:28:06 +0900

    테스트

--- src/test/java/org/sparta/springtask/SpringTaskApplicationTests.java
+++ src/test/java/org/sparta/springtask/SpringTaskApplicationTests.java
@@ -1,13 +1,9 @@
 package org.sparta.springtask;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@SpringBootTest
 class SpringTaskApplicationTests {
 
     @Test
