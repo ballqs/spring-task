a43d3c704d5b95c0279dacffca8160f013f31911
ballqs <46590521+ballqs@users.noreply.github.com>
2024-10-22 00:00:25 +0900

    redis 추가

--- src/test/java/org/sparta/springtask/SpringTaskApplicationTests.java
+++ src/test/java/org/sparta/springtask/SpringTaskApplicationTests.java
@@ -1,9 +1,13 @@
 package org.sparta.springtask;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SpringTaskApplicationTests {
 
     @Test
